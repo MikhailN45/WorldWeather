800721018c4876d66cfc3e6f4feddc48bae94b36
MikhailN45 <shelove6391@gmail.com>
2025-04-06 11:57:11 +0300

    add room, change Moshi to Gson

---
+++ app/src/main/java/com/naumov/worldweather/data/database/AppDatabase.kt
@@ -0,0 +1,20 @@
+package com.naumov.worldweather.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.naumov.worldweather.data.database.dao.WeatherDao
+import com.naumov.worldweather.data.database.dao.WeatherEntity
+import com.naumov.worldweather.data.database.dao.WeatherTypeAdapter
+
+//@Database(entities = [WeatherEntity::class], version = 1, exportSchema = false)
+@TypeConverters(
+    WeatherTypeAdapter::class
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun weatherDao(): WeatherDao
+
+    companion object {
+        const val DATABASE = "room_database"
+    }
+}
